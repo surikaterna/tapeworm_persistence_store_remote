d53c8d4588a2ec15259b50e1859ffcaa622aa960
Thomas <tbruun@gmail.com>
2018-12-14 11:53:41 +0100

    fixes test cases not passing

--- test/stream_sub.js
+++ test/stream_sub.js
@@ -3,51 +3,54 @@ var should = require('should');
 var EventStreamSubscriber = require('..').Client.EventStreamSubscriber;
 
 function getSubscriber() {
-	var client = {subscribe: function() {return {stop:function() {}}}};
- 	return new EventStreamSubscriber(client);
+  var client = {subscribe: function() {return {stop:function() {}}}, request: function (opts, cb) { cb(null, {commits:[]})}};
+  var versionProvider = function () {
+    return Promise.resolve(0);
+  };
+  return new EventStreamSubscriber(client, versionProvider, function () {});
 }
 
 describe('EventStreamsubhronizer', function() {
-	it("#subscribe should return a handle", function() {
-		var sub = getSubscriber();
-		var handle = sub.subscribe('stream-1');
-		handle.should.be.ok;
-	});
-	it("#subscribe should return a handle which you can use to stop subscription", function() {
-		var sub = getSubscriber();
-		var handle = sub.subscribe('stream-1');
-		handle.stop.should.be.ok
-	});	
-	it("#subscribe stop should remove subscriptions", function() {
-		var sub = getSubscriber();
-		var handle = sub.subscribe('stream-1');
-		sub.activeStreams().length.should.equal(1);
-		handle.stop();
-		sub.activeStreams().length.should.equal(0);
-	});	
-	it("#subscribe stop multiple times should throw", function() {
-		var sub = getSubscriber();
-		var handle = sub.subscribe('stream-1');
-		handle.stop();
-		should.throws(function() { handle.stop(); });
-	});	
-	it("#subscribe multiple times should not increase activeStreams", function() {
-		var sub = getSubscriber();
-		var handle = sub.subscribe('stream-1');
-		var handle2 = sub.subscribe('stream-1');
-		sub.activeStreams().length.should.equal(1);
-	});	
-	it("#stop with multiple subscriptions should not decrease activeStreams", function() {
-		var sub = getSubscriber();
-		var handle = sub.subscribe('stream-1');
-		var handl2 = sub.subscribe('stream-1');
-		handle.stop();
-		sub.activeStreams().length.should.equal(1);
-	});	
-	it("#subscribe on multiple streams increases activeStreams", function() {
-		var sub = getSubscriber();
-		var handle = sub.subscribe('stream-1');
-		var handl2 = sub.subscribe('stream-2');
-		sub.activeStreams().length.should.equal(2);
-	});	
+  it("#subscribe should return a handle", function() {
+    var sub = getSubscriber();
+    var handle = sub.subscribe('stream-1');
+    handle.should.be.ok;
+  });
+  it("#subscribe should return a handle which you can use to stop subscription", function() {
+    var sub = getSubscriber();
+    var handle = sub.subscribe('stream-1');
+    handle.stop.should.be.ok
+  });
+  it("#subscribe stop should remove subscriptions", function() {
+    var sub = getSubscriber();
+    var handle = sub.subscribe('stream-1');
+    sub.activeStreams().length.should.equal(1);
+    handle.stop();
+    sub.activeStreams().length.should.equal(0);
+  });
+  it("#subscribe stop multiple times should throw", function() {
+    var sub = getSubscriber();
+    var handle = sub.subscribe('stream-1');
+    handle.stop();
+    should.throws(function() { handle.stop(); });
+  });
+  it("#subscribe multiple times should not increase activeStreams", function() {
+    var sub = getSubscriber();
+    var handle = sub.subscribe('stream-1');
+    var handle2 = sub.subscribe('stream-1');
+    sub.activeStreams().length.should.equal(1);
+  });
+  it("#stop with multiple subscriptions should not decrease activeStreams", function() {
+    var sub = getSubscriber();
+    var handle = sub.subscribe('stream-1');
+    var handl2 = sub.subscribe('stream-1');
+    handle.stop();
+    sub.activeStreams().length.should.equal(1);
+  });
+  it("#subscribe on multiple streams increases activeStreams", function() {
+    var sub = getSubscriber();
+    var handle = sub.subscribe('stream-1');
+    var handl2 = sub.subscribe('stream-2');
+    sub.activeStreams().length.should.equal(2);
+  });
 });
