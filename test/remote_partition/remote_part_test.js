673875cdc8c7dcc32baf193837c3661b851c28d6
Thomas <tbruun@gmail.com>
2018-12-17 10:57:53 +0100

    adds test cases and `queryStream` implementation

--- test/remote_partition/remote_part_test.js
+++ test/remote_partition/remote_part_test.js
@@ -10,7 +10,7 @@ var autobus = {
   leave: function () {}
 };
 
-describe('Remote server/client', function () {
+describe('Remote partition server/client', function () {
   var socketServer, socketClient, clientTapeworm, tapeworm, serverPartition = null, clientPartition = null;
 
   beforeEach(function (done) {
@@ -43,11 +43,36 @@ describe('Remote server/client', function () {
     socketServer.emit('ping', 'Hello');
   });
 
-  it.only('#server should work', function (done) {
+  it('#loadSnapshot should work on remote partition', function (done) {
     const tapewormSyncServer = new TapewormSyncServer(serverPartition, autobus);
     serverPartition.storeSnapshot('1', { test: 'success' }, 2, function () {
       clientPartition.loadSnapshot('1', function (err, res) {
         res.snapshot.test.should.equal('success');
+        done();
+      });
+    });
+    tapewormSyncServer.addSocket(socketServer);
+  });
+  it('#queryStream should work on remote partition', function (done) {
+    var commits = [new Commit('1', 'blondie', '1', 0, [{ id: '1', type: 'registered' }]), new Commit('2', 'blondie', '1', 1, [{ id: '1', type: 'amended' }])];
+    const tapewormSyncServer = new TapewormSyncServer(serverPartition, autobus);
+    serverPartition.append(commits).then(function () {
+      clientPartition.queryStream('1', function (err, res) {
+        res.length.should.equal(2);
+        res[0].events[0].type.should.equal('registered');
+        res[1].events[0].type.should.equal('amended');
+        done();
+      });
+    });
+    tapewormSyncServer.addSocket(socketServer);
+  });
+  it('#queryStream should work on remote partition from commit sequence', function (done) {
+    var commits = [new Commit('1', 'blondie', '1', 0, [{ id: '1', type: 'registered' }]), new Commit('2', 'blondie', '1', 1, [{ id: '1', type: 'amended' }])];
+    const tapewormSyncServer = new TapewormSyncServer(serverPartition, autobus);
+    serverPartition.append(commits).then(function () {
+      clientPartition.queryStream('1', 0, function (err, res) {
+        res.length.should.equal(1);
+        res[0].events[0].type.should.equal('amended');
         done();
       });
     });
