5471c092dc70bdec7033dd32475bc2e57c63b926
Thomas <tbruun@gmail.com>
2018-12-18 10:59:54 +0100

    adds suport for creating client outside of agent.js, adds support for promise on `loadSnapshot` and `queryStream`

--- test/remote_partition/remote_part_test.js
+++ test/remote_partition/remote_part_test.js
@@ -53,11 +53,35 @@ describe('Remote partition server/client', function () {
     });
     tapewormSyncServer.addSocket(socketServer);
   });
+  it('#loadSnapshot should work on remote partition with promise', function (done) {
+    const tapewormSyncServer = new TapewormSyncServer(serverPartition, autobus);
+    serverPartition.storeSnapshot('1', { test: 'success' }, 2, function () {
+      clientPartition.loadSnapshot('1').then(function (res) {
+        res.snapshot.test.should.equal('success');
+        done();
+      });
+    });
+    tapewormSyncServer.addSocket(socketServer);
+  });
   it('#queryStream should work on remote partition', function (done) {
     var commits = [new Commit('1', 'blondie', '1', 0, [{ id: '1', type: 'registered' }]), new Commit('2', 'blondie', '1', 1, [{ id: '1', type: 'amended' }])];
     const tapewormSyncServer = new TapewormSyncServer(serverPartition, autobus);
     serverPartition.append(commits).then(function () {
       clientPartition.queryStream('1', function (err, res) {
+        res.length.should.equal(2);
+        res[0].events[0].type.should.equal('registered');
+        res[1].events[0].type.should.equal('amended');
+        done();
+      });
+    });
+    tapewormSyncServer.addSocket(socketServer);
+  });
+  it('#queryStream should work on remote partition with promise', function (done) {
+    var commits = [new Commit('1', 'blondie', '1', 0, [{ id: '1', type: 'registered' }]), new Commit('2', 'blondie', '1', 1, [{ id: '1', type: 'amended' }])];
+    const tapewormSyncServer = new TapewormSyncServer(serverPartition, autobus);
+    serverPartition.append(commits).then(function () {
+      clientPartition.queryStream('1').then(function (res) {
+        console.log('res', res);
         res.length.should.equal(2);
         res[0].events[0].type.should.equal('registered');
         res[1].events[0].type.should.equal('amended');
